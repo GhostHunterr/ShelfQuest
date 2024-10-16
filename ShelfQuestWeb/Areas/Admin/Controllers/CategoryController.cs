b39882f837335973db63fc68311c110920eb263c
Farhan <farhansarang7@gmail.com>
2024-10-15 21:21:49 -0400

    Added  asp-area to the _Layout.

---
+++ ShelfQuestWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShelfQuest.DataAccess.Data;
+using ShelfQuest.DataAccess.Repository;
+using ShelfQuest.DataAccess.Repository.IRepository;
+using ShelfQuest.Models;
+
+namespace ShelfQuestWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.DisplayOrder.ToString() == obj.Name)
+            {
+                ModelState.AddModelError("Name", "Name and Display Order cannot be of same Value.");
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category created Successfully";
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _unitOfWork.Category.GetFirstorDefault(u => u.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category updated Successfully";
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View(obj);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _unitOfWork.Category.GetFirstorDefault(u => u.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _unitOfWork.Category.GetFirstorDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Category.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Category deleted Successfully";
+            return RedirectToAction("Index", "Category");
+        }
+
+
+    }
+}
